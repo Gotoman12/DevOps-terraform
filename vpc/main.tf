c41f09d3ecbd8b395324888ad82a32e308532620
Mslliksrjun <mallikarjunckm@example.com>
2025-12-25 17:28:51 +0530

    availability_zone

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "project_java" {
 resource "aws_subnet" "project_java-subnet-public1" {
   vpc_id = aws_vpc.project_java.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = ["us-east-1a","us-east-1b"]
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name: "project-java-public-subnet"
@@ -25,7 +25,7 @@ resource "aws_subnet" "project_java-subnet-public1" {
 resource "aws_subnet" "project_java-subnet-public2" {
   vpc_id = aws_vpc.project_java.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = ["us-east-1a","us-east-1b"]
+  availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
     Name: "project-java-private-subnet"
