6830fe1e6df1b2ad9e7ed50fb18dba320d55636f
Mslliksrjun <mallikarjunckm@example.com>
2026-01-01 18:45:18 +0530

    added

--- main.tf
+++ main.tf
@@ -78,12 +78,13 @@ ingress {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-    egress = {
-        protocol = "-1"
-        from_port = 0
-        to_port = 0
-        cidr_blocks	= ["0.0.0.0/0"]
-    }
+    egress = [{
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+}]
+
 
      tags = {
     Name = "App-SG"
