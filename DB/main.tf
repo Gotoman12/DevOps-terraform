f0b619a2ff615f0e60e808dd5e592bd00444d58a
Mslliksrjun <mallikarjunckm@example.com>
2026-01-02 12:42:37 +0530

    added

--- DB/main.tf
+++ DB/main.tf
@@ -2,12 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "db_password" {
+  description = "RDS admin password"
+  type        = string
+  sensitive   = true
+}
+
 resource "aws_db_instance" "example" {
-    engine              = "mysql"
-    allocated_storage   = 10
-    instance_class      = "db.t4g.micro"
-    db_name                = "database-1"
-    username            = "admin"
-    password            = "${var.db_password}"
-    skip_final_snapshot = true
+  identifier          = "mysql-db"
+  engine              = "mysql"
+  engine_version      = "8.0"
+  allocated_storage   = 20
+  instance_class      = "db.t3.micro"
+
+  db_name             = "database1"
+  username            = "admin"
+  password            = var.db_password
+
+  skip_final_snapshot = true
+  publicly_accessible = true
 }
