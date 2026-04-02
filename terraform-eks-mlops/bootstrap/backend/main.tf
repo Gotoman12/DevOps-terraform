951842b83a18211c047e200986390df5a10283a0
Arjun <mallikarjunckm@gmail.com>
2026-04-02 11:50:01 +0000

    s3 added

--- terraform-eks-mlops/bootstrap/backend/main.tf
+++ terraform-eks-mlops/bootstrap/backend/main.tf
@@ -36,6 +36,22 @@ attributes = {
   }
 }
 
-resource "aws_s3_bucket" "s3-bucket" {
+resource "aws_s3_bucket" "tf_state" {
     bucket = "arjun-${random_string.s3-string.result}"
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
