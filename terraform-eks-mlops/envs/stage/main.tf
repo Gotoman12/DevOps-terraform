cb0564a62044d6cb8fc80cb927df7b80085abafd
Arjun <mallikarjunckm@gmail.com>
2026-04-07 07:34:45 +0000

    jenkins added

--- terraform-eks-mlops/envs/stage/main.tf
+++ terraform-eks-mlops/envs/stage/main.tf
@@ -35,7 +35,7 @@ module "network" {
 
    
   instance_types = var.instance_types
-  desired_size   = var.desired
+  desired_size   = var.desired_size
   min_size       = var.min_size
   max_size       = var.max_size
 }
@@ -57,9 +57,9 @@ data "aws_eks_cluster_auth" "this" {
 }
 
 provider "kubernetes" {
-  host = data.aws_eks_cluster.this.endpoint
-  client_certificate = filebase64(data.aws_eks_cluster_auth.this.certificate_authority[0].data)
-  token = data.aws_eks_cluster_auth.this.token
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 module "k8s-app" {
--- terraform-eks-mlops/envs/stage/output.tf
+++ terraform-eks-mlops/envs/stage/output.tf
@@ -6,4 +6,3 @@ output "ecr_url" {
   value = module.ecr.repository_url
 }
 
-output "namespace" { value = module.k8s_app.namespace }
